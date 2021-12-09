b161f65eb23d73cc7eb56f52d04e713e9023c4e1
Jessica Lindsey <Jessicalindsey2@gmail.com>
2021-12-09 14:27:02 -0700

    references table

--- scriptsContact.js
+++ scriptsContact.js
@@ -1,33 +1,77 @@
-let refs = [
+const refs = [
     {
-        name: "Darin Ramos",
-        phone: "801.652.xxxx",
+        name: "Darin Robin",
+        phone: "801.652.0000",
         email: "darinramos@test.com",
         relationship: "Client"
     },
 
     {
-        name: "Zuzana Stehlikova",
-        phone: "801.205.xxxx",
+        name: "Zuzana Stella",
+        phone: "801.205.0000",
         email: "zuzana@gmail.com",
         relationship: "Employer"
     },
 
     {
-        name: "Kyle Challis",
-        phone: "801.839.xxxx",
+        name: "Kyle Lindsey",
+        phone: "801.831.0000",
         email: "kyle@gmail.com",
         relationship: "Personal"
     }
 ];
 
-var referenceList = "<ul>";
+let referenceList = "<ul>";
 
 for (i = 0; i < refs.length; i++) {
     let contact = refs[i];
     referenceList += "<li>" + contact.name + " | " + contact.phone + " | " + contact.email + " | " + contact.relationship + "</li>";
 }
-
 referenceList += "</ul>";
 
 document.getElementById("references").innerHTML = referenceList;
+
+const references = [
+    {
+        first: "Darin",
+        last: "Robin",
+        relation: "Client"
+    },
+
+    {
+        first: "Zuzana",
+        last: "Stella",
+        relation: "Employer"
+    },
+
+    {
+        first: "Kyle",
+        last: "Lindsey",
+        relation: "Brother"
+    }
+]
+
+let tbReferences = document.getElementById('tbReferences')
+
+references.forEach((reference, index) => {
+    let referenceTr = document.createElement('tr');
+    let first = document.createElement('td');
+    let last = document.createElement('td');
+    let relation = document.createElement('td');
+    // number.innerHTML = index;
+    first.innerHTML = reference.first;
+    last.innerHTML = reference.last;
+    relation.innerHTML = reference.relation;
+    // referenceTr.append(number);
+    referenceTr.append(first);
+    referenceTr.append(last);
+    referenceTr.append(relation);
+    tbReferences.append(referenceTr);
+})
+
+
+
+
+
+
+
---
+++ test.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+$foobar = 'world';
+echo "hello, {$foobar}!";
+?>
+
+</body>
+</html>
+
+
+
+
+<html>
+<body>
+
+hello, world!
+
+</body>
+</html>
